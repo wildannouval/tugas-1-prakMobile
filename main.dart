e36c358ecfd44dece0abdfb833f073a75844a966
Wildan Nouval Rizki <wildannouvalrizki@gmail.com>
2023-09-10 20:57:00 +0700

    Jawab tugas

--- main.dart
+++ main.dart
@@ -6,10 +6,13 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = DateTime.now().year - tahunLahir;
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021017", "Wildan Nouval Rizki", 2003);
+  mhs.perkenalan();
 }
